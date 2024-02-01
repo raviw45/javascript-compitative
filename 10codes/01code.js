37cbf60ad2eee081be8f27a4a5cee0a94bf23765
Ravi Waghmare <122720689+raviw45@users.noreply.github.com>
2024-02-01 14:52:36 +0000

    js

---
+++ 10codes/01code.js
@@ -0,0 +1,20 @@
+/*
+
+1.What are the possible ways to create the database?
+   a.Object Literal syntax
+   b.Object constructor
+   c.Object's create method
+   d.Function constructor
+   e.Function construction with prototype
+   f.ES6 class syntax
+   g.Singleton pattern
+
+*/
+
+var Object={
+    name:"Ravikant",
+    age:25
+}
+
+console.log(Object.name);
+
